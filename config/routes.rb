208bec9489f084bb7ac05c59e771e8a4c0212a7b
Tom Allen <tomallen_1@hotmail.co.uk>
2023-04-22 16:27:03 +1000

    added searches controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,8 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  root to: "pages#search"
+  root to: "searches#index"
   # Defines the root path route ("/")
-
-
-
+  get "/about", to: "pages#about"
   # root "articles#index"
 end
