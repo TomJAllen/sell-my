fbe6f1e5a88a55da42cfe8d47334f66ee2dc1a80
Tom Allen <tomallen_1@hotmail.co.uk>
2023-04-28 10:49:06 +1000

    added swwop search

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -7,14 +7,30 @@ class SearchesController < ApplicationController
 
   def search
     # search = params[:search] # initial search from index page
-    search = params[:search]
-    @results = []
-    html_content = URI.open("https://www.depop.com/search/?q=#{search}").read
+    @search = params[:search]
+    @depop_search = depop_search
+    @swop_search = swop_search
+  end
+
+  def depop_search
+    @depop_results = []
+    html_content = URI.open("https://www.depop.com/search/?q=#{@search}").read
     doc = Nokogiri::HTML.parse(html_content)
 
     doc.search('[data-testid="product__items"] [data-testid="product__item"]').each_with_index do |element, index|
       link = element.get_attribute('href')
-      @results << "#{index + 1}. #{link}"
+      @depop_results << "#{index + 1}. #{link}"
+    end
+  end
+
+  def swop_search
+    @swop_results = []
+    html_content = URI.open("https://swop.net.au/search?q=#{@search}").read
+    doc = Nokogiri::HTML.parse(html_content)
+
+    doc.search('.full-unstyled-link').each_with_index do |element, index|
+      link = element.get_attribute('href')
+      @depop_results << "#{index + 1}. #{link}"
     end
   end
 
